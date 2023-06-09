3aa057305bb6939e1642dd2499ac9f3518169dfd
dingnickhong <nickhongding@hotmail.com>
2023-06-10 02:58:35 +0800

    style: navbar tweak

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ className, children }: any) => {
   return (
     <div
       className={cn(
-        "flex flex-col gap-16 p-4 sm:p-8 md:py-16 h-full",
+        "flex flex-col gap-6 sm:gap-16 p-4 sm:py-16 h-full",
         "w-full max-w-full lg:max-w-screen-xl mx-auto"
       )}
     >
